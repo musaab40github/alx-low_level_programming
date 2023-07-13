0a8a9cdf326fe48e0141dc3490cf79f532f02f64
musaab40github <musaabdulkadir01@gmail.com>
2023-07-14 00:39:08 +0100

    c programming task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - printf
+ *
+ * Description: Hello word
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - hello
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Hi friend\n");
+	return (0);
+}
