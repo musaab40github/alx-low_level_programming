d9220c8f3758aab8c71a983d1932120c9daaffe6
musaab40github <musaabdulkabir01@gmail.com>
2023-07-16 06:22:52 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 99; i++)
+	for (i = 0; i <= 98; i++)
 		for (j = i + 1; j <= 99; j++)
 		{
 			putchar(i / 10 + '0');
@@ -18,11 +18,10 @@ int main(void)
 			putchar(' ');
 			putchar(j / 10 + '0');
 			putchar(j % 10 + '0');
-			if (i != 99 || j != 99)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			if (i != 98 ||  j != 99)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	putchar('\n');
 	return (0);
