f9155645e1f70f9fba9e439b9394939ed41d8385
musaab40github <musaabdulkabir01@gmail.com>
2023-08-01 12:34:19 +0100

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,14 +2,14 @@
 
 /**
  * _memset - function that fills memory with a constant byte
- * @s: the pointer 
+ * @s: the pointer
  * @b: the placeholder
  * @n: the number of iteration
  * Return: 1
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i; 
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
