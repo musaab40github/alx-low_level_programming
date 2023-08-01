43e607b415251983b0c252e52ff2bbf310bda9e2
musaab40github <musaabdulkabir01@gmail.com>
2023-08-01 14:17:32 +0100

    finishing point

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,19 +1,28 @@
 #include "main.h"
 
 /**
- * _strspn - function that gets the length of a prefix substring
- * @s: pointer to the inigital segment
- * @accept: pointer to the matcher
- * Return: 1
+ * _strspn - gets the length of a prefix substring.
+ * @s: initial segment.
+ * @accept: accepted bytes.
+ * Return: the number of accepted bytes.
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int count = 0;
+	unsigned int i, j, bool;
 
-	while (*s != '\0' && strchr(accept, *s) != NULL)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		count++;
-		s++;
+		bool = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				bool = 0;
+				break;
+			}
+		}
+		if (bool == 1)
+			break;
 	}
-	return (count);
+	return (i);
 }
