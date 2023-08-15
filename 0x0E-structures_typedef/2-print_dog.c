3500065a7207f933cc54cd5445118f6cbe767fc3
Abdulkabir Musa <120343852+musaab40github@users.noreply.github.com>
2023-08-15 10:33:18 +0100

    new files added

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,32 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - function that prints a struct dog
+ * @d: pointer to the struct dog
+ *
+ * Return: nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if ((*d).name == NULL)
+	{
+		(*d).name = "(nil)";
+	}
+	if ((*d).age == 0.0)
+	{
+		(*d).age = 0.0;
+	}
+	if ((*d).owner == NULL)
+	{
+		(*d).owner = "(nil)";
+	}
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
